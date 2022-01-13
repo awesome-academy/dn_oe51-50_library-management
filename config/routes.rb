17fe4582622856dcf17cfed4acd0b8c9f0a0e41d
ThienPhuoc1002 <thienphuocho1002@gmail.com>
2022-01-13 08:48:42 +0700

    Show and sort books

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/contact", to: "static_pages#contact"
-    resources :books, only: :show
+    resources :books, only: %i(index show)
 
     namespace :admin do
       resources :books
