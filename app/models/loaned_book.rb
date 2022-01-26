b505dc6a226a6b21db64cbc7fb83f040e1af4576
TommyHoang2911 <hoangcongduyhy@gmail.com>
2022-01-26 14:04:50 +0700

    [Task#45554] Assign pending status when user request loan books

--- app/models/loaned_book.rb
+++ app/models/loaned_book.rb
@@ -1,4 +1,7 @@
 class LoanedBook < ApplicationRecord
+  include SessionsHelper
+
+  before_create :set_pending
   enum status: {rending: 0, returned: 1, overdated: 2, pending: 3, rejected: 4, closed: 5}
 
   belongs_to :user
@@ -19,4 +22,12 @@ class LoanedBook < ApplicationRecord
   validates :quantity,
             presence: true,
             numericality: {in: Settings.range.digit_1..Settings.range.digit_3}
+
+  private
+
+  def set_pending
+    return unless current_user.member?
+
+    self.status = LoanedBook.statuses[:pending]
+  end
 end
--- app/models/loaned_detail.rb
+++ app/models/loaned_detail.rb
@@ -1,6 +1,9 @@
 class LoanedDetail < ApplicationRecord
+  include SessionsHelper
+
+  before_create :set_pending
   after_create :change_book_quantity_down
-  enum status: {rending: 0, returned: 1, overdated: 2, pending: 3, rejected: 4, losted: 3}
+  enum status: {rending: 0, returned: 1, overdated: 2, pending: 3, rejected: 4, losted: 5}
 
   belongs_to :book
   belongs_to :loaned_book
@@ -8,6 +11,14 @@ class LoanedDetail < ApplicationRecord
   private
 
   def change_book_quantity_down
+    return if pending?
+
     book.update!(quantity: book.quantity - quantity)
+  end
+
+  def set_pending
+    return unless current_user.member?
+
+    self.status = LoanedDetail.statuses[:pending]
   end
 end
